be99b7b7c6f5c52b296e711111179f08c064cb93
bredele <olivier.wietrich@gmail.com>
2013-10-16 14:09:46 -0600

    add todo: do compare component

--- test/last.js
+++ test/last.js
@@ -15,6 +15,7 @@ describe('Last', function(){
   it('should return the n last element of an Array', function(){
     var arr = ['olivier', 'bruno', 'amy'];
     var result = last(arr, 2);
+    //TODO: do compare component
     assert(result.length === 2);
     assert(result[0] === 'bruno');
     assert(result[1] === 'amy');
